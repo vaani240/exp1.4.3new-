8b1ac675580be4d403f0c27464f1b904e6682564
vaani57 <your-email@example.com>
2026-03-04 13:51:20 +0530

    ready for deployment

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,13 @@
 import { createClient } from 'redis';
 
-// Create a Redis client instance
-const redisClient = createClient();
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+});
 
-// Handle Redis connection errors
 redisClient.on('error', (err) => {
     console.error('Redis Error:', err);
 });
 
-// Function to connect to Redis
 const connectRedis = async () => {
     await redisClient.connect();
     console.log('✅ Redis Connected');
